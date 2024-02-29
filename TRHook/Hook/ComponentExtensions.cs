419afa0775c7ffa1982c61b33ad312dbcb1757ba
Danilo <danilogmoura@gmail.com>
2024-02-29 19:31:59 -0300

    Cheating o valor das armas. Valor fixo igual a 1.

---
+++ TRHook/Hook/ComponentExtensions.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace TRHook.Hook
+{
+    [HarmonyPatch(typeof(ComponentExtensions), "GetWeaponUpgradePrice")]
+    public class ComponentExtensionsPath_GetWeaponUpgradePrice
+    {
+        public static bool Prefix(BigNum __result)
+        {
+            __result = 1;
+            return false;
+        }
+    }
+}
