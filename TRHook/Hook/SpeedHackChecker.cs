6f949321cbb0aa4085a760f3ba63bb9e65a57e73
Danilo <danilogmoura@gmail.com>
2024-02-29 19:00:28 -0300

    Desativando o HackCheker

---
+++ TRHook/Hook/SpeedHackChecker.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace TRHook.Hook
+{
+    [HarmonyPatch(typeof(SpeedHackChecker), "Start", typeof(float), typeof(byte), typeof(int))]
+    public static class SpeedHackChecker_Start
+    {
+        private static bool Prefix()
+        {
+            MelonLogger.Msg("SpeedHackChecker.Start was called: Disabling SpeedHackChecker.");
+            return false;
+        }
+    }
+}
